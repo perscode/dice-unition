74e1e42ffe9bf8afcf91ea21b80e30923f969da6
Per Johansson <per.johansson@bimobject.com>
2022-07-16 16:28:16 +0200

    webgl

---
+++ Assets/Scripts/SetBarValue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetBarValue : MonoBehaviour
+{
+    public RectTransform RectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rect temp = RectTransform.rect;
+        //temp.width = 225;
+        RectTransform.sizeDelta = new Vector2 (45, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
