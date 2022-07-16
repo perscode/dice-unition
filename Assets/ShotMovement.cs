077d9ca81faa025bb92b267ce2622ffbbe096bd1
Per Johansson <per.johansson@bimobject.com>
2022-07-16 11:36:22 +0200

    move content of sprites2 to sprites.save proj.

--- Assets/ShotMovement.cs
+++ Assets/ShotMovement.cs
@@ -6,13 +6,6 @@ public class ShotMovement : MonoBehaviour
 {
     public Vector2 bulletVelocity;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector2 pos = transform.position;
